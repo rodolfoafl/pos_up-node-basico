f511dc89a8d6b634d1a62aaa1b9f8e862cd3aecc
Rodolfo <27327749+rodolfoafl@users.noreply.github.com>
2021-07-31 20:06:04 -0300

    Updated read & update

--- express-mysql/services/pets.js
+++ express-mysql/services/pets.js
@@ -2,7 +2,7 @@ const db = require("./db");
 const helper = require("../helper");
 
 const read = async () => {
-  const rows = await db.query("SELECT * FROM pet");
+  const rows = await db.query("SELECT name, age, image FROM pet");
   const data = helper.emptyOrRows(rows);
 
   return {
@@ -12,8 +12,6 @@ const read = async () => {
 
 const validateCreate = (pet) => {
   let messages = [];
-
-  console.log(pet);
 
   if (!pet) {
     messages.push("No object is provided");
@@ -67,11 +65,19 @@ const setUpdatedString = (pet) => {
   return string;
 };
 
-const update = async (id, pet) => {
+const validateUpdate = (pet) => {
   let messages = [];
 
   if (!pet) {
     messages.push("No object is provided");
+  }
+
+  if (pet.name && pet.name.length === 0) {
+    messages.push("Name is empty");
+  }
+
+  if (pet.age && !Number.isInteger(pet.age)) {
+    messages.push("Age it not a number");
   }
 
   if (messages.length) {
@@ -80,6 +86,10 @@ const update = async (id, pet) => {
 
     throw error;
   }
+};
+
+const update = async (id, pet) => {
+  validateUpdate(pet);
 
   let updateString = setUpdatedString(pet);
 
@@ -88,11 +98,16 @@ const update = async (id, pet) => {
 
   let message = `Error in updating pet: ${result.info}`;
 
+  let updatePet = {};
   if (result.affectedRows) {
     message = "Pet information updated successfully";
+    updatePet = await db.query(
+      "SELECT name, age, image FROM pet WHERE ID = ?",
+      [id]
+    );
   }
 
-  return { message };
+  return { message, updatePet };
 };
 
 module.exports = {
