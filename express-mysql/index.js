891443bc5e9a23c0d889df5fe05312b90c16483a
Rodolfo <27327749+rodolfoafl@users.noreply.github.com>
2021-08-17 00:52:34 -0300

    Added index.js

--- express-mysql/index.js
+++ express-mysql/index.js
@@ -1,22 +1,31 @@
 const express = require("express");
-
-const petsRoutes = require("./routes/pets");
+const yaml = require("js-yaml");
+const fs = require("fs-extra");
+const router = require("./routes");
 
 const app = express();
-const port = 3000;
+let port = "";
+
+try {
+  let fileContents = fs.readFileSync("./config/dev.yaml", "utf8");
+  data = yaml.load(fileContents);
+  port = data["port"] || 3000;
+} catch (error) {
+  console.error(error);
+}
 
 app.use(express.json());
 
-app.use("/pets", petsRoutes);
+app.use(router);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
+  console.error(err.message);
   res.status(statusCode).json({ message: err.message });
 
   return;
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`);
 });
